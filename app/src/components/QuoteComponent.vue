108a0c8eeefa34927c0dd84d6b615636129429f9
Rita <shaidrv@gmail.com>
2023-03-27 12:36:10 +0700

    Вывела разницу в процентах между начальной и текущей котировкой
    + округлила текущую котировку через computed свойство; динамическое изменение цвета текста в зависимости от знака разницы

--- app/src/components/QuoteComponent.vue
+++ app/src/components/QuoteComponent.vue
@@ -3,10 +3,13 @@
     <ul>
       <li>{{ quote.name }}</li>
       <li>
-        {{ quote.currentCourse }}
+        {{ currentCourse }}
         $
       </li>
-      <li>{{ initialCourse }}</li>
+      <li :style="{ color: difference < 0 ? '#FF3165' : '#78CBBB' }">
+        {{ difference }}
+        %
+      </li>
     </ul>
   </div>
 </template>
@@ -14,6 +17,21 @@
 <script>
 export default {
   props: ["quote", "initialCourse"],
+  computed: {
+    currentCourse() {
+      let current = +this.quote.currentCourse;
+      return current > 1 ? current.toFixed(2) : current.toFixed(5);
+    },
+    difference() {
+      let current = this.quote.currentCourse;
+      let initial = this.initialCourse;
+      if (current !== initial) {
+        return (((current - initial) / current) * 100).toFixed(2);
+      } else {
+        return "0";
+      }
+    },
+  },
 };
 </script>
 
